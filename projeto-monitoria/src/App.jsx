84b77cb4310d488ab81168b591e3f97b7344210e
Felipe Sutter <lipeps61@gmail.com>
2023-10-26 18:04:40 -0300

    projeto monitoria newsletter

---
+++ projeto-monitoria/src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Que tal comprar uma mansão em Hong Kong por US$ 166 milhões?</h1>
+      <br />
+      <div className="subtitulo">
+        <small>EXTRA</small>
+      </div>
+      <img
+        src="https://uploads.metropoles.com/wp-content/uploads/2022/03/10113427/1-Casa-mais-cara-do-mundo_1.jpg"
+        alt="Mansão"
+        className="imagem"
+      />
+      <p className="rotulo-img">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam, modi
+        dignissimos! Eos enim quae voluptatem voluptate nihil perspiciatis.
+        Facilis eveniet nisi vitae deserunt culpa! Rerum reiciendis alias
+        placeat unde quaerat!
+      </p>
+    </>
+  );
+}
+
+export default App;
